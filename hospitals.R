d2f5c8e73b7af21c50388f5a0b482f0d444a4955
Juan Alvarado <juan.alvarado.1381@gmail.com>
2015-05-28 10:41:01 -0400

    building outcome functions validation

--- hospitals.R
+++ hospitals.R
@@ -8,15 +8,15 @@ best <- function(state, outcome) {
 
 	outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 	outcome_data[, 11] <- as.numeric(outcome_data[, 11])
-	valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
+	valid_outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
 	valid_states <- levels(factor(outcome_data[, 7]))
 
 ## Check that state and outcome are valid
-	print(state)
-	print(valid_states)
+	##print(state)
+	##print(valid_states)
 
-	print(outcome)
-	print(valid_outcomes)
+	##print(outcome)
+	##print(valid_outcomes)
 
 	isValidState <- function ( s ){
 		for (vs in valid_states){
@@ -32,11 +32,18 @@ best <- function(state, outcome) {
 	}
 
 	if (!isValidState(state))
-		stop(c("invalid state", state))
+		stop(c("invalid state"))
 
 	if (!isValidOutcome(outcome))
-		stop(c("invalid outcome", outcome ))
+		stop(c("invalid outcome" ))
 
 ## Return hospital name in that state with lowest 30-day death
 ## rate
+stateh <- outcome_data[,7] == state
+valid_values<-!is.na(outcome_data[,11][stateh])
+valid_rates <- outcome_data[,11][stateh][valid_values]
+min_death<-min(valid_rates)
+c(outcome_data[,2][valid_values],outcome_data[,11][valid_values])
+
+
 }
